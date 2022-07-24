9a3e50740e484129570c6034a6c3bdf33ae39a17
mthaisa <109315681+mthaisa@users.noreply.github.com>
2022-07-23 21:07:26 -0300

    CM - Inicializando repositório + Criação de Fragments do Produto.

---
+++ app/src/main/java/com/agendamento/handsonwork/produto/AdicionarFragment.java
@@ -0,0 +1,30 @@
+package com.agendamento.handsonwork.produto;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.agendamento.handsonwork.R;
+
+public class AdicionarFragment extends Fragment {
+
+    public AdicionarFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.produto_fragment_adicionar, container, false);
+    }
+}
